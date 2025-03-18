f705c48668b726d59b17840f623f68aad531c7e6
IrvinH <irvindhp@gmail.com>
2025-03-18 17:59:58 -0600

    Función que agrega amigos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//Función que agrega amigos
+function agregarAmigo()
+{
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    //Valida si el nombre esta vacío o si tiene algún número
+    if (nombreAmigo.trim() === "" || /\d/.test(nombreAmigo))
+    {
+        alert("Por favor, inserte un nombre válido.")
+    }
+    else
+    {
+        amigos.push(nombreAmigo);
+    }
+
+    //Llamamos a la función para restablecer el cuadro de texto a una cadena vacía
+    limpiarNombre();
+}
+
+//Función que restablece el cuadro de texto a una cadena vacía
+function limpiarNombre()
+{
+    document.querySelector('#amigo').value = "";
+}
