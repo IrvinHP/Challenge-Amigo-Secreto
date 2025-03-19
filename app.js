60e28ce446d8ce6ffbb3041b43e5fb7cca972889
IrvinH <irvindhp@gmail.com>
2025-03-18 18:57:12 -0600

    Implementación de función que sortea los amigos

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ function agregarAmigo()
         amigos.push(nombreAmigo);
     }
 
+    // Llamamos a la función para actualizar la lista
+    actualizarLista();
+    
     // Llamamos a la función para restablecer el cuadro de texto a una cadena vacía
     limpiarNombre();
 }
@@ -48,4 +51,24 @@ function actualizarLista()
         // Agregar el elemento <li> a la lista
         lista.appendChild(elemento);
     }
+}
+
+// Función para sortear los amigos
+function sortearAmigos()
+{
+    // Validamos si la lista de amigos esta vacía o tiene menos de dos integrantes
+    if (amigos.length <= 1)
+    {
+        alert("La lista de amigos está vacía. Por favor, agrega al menos dos amigos antes de realizar el sorteo.");
+        return; // Salir de la función si no hay elementos
+    }
+    
+    // Generar un índice aleatorio
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    // Obtener el nombre sorteado
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    // Mostrar el resultado en un elemento HTML con el ID "resultado"
+    document,getElementById("resultado").innerHTML = `El amigo seleccionado es: <strong>${amigoSorteado}</strong>`;
 }
